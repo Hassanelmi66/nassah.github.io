fcd5010f73ab2865ed40e9e0dea5b1d1394f6729
Hassanelmi66 <89681325+Hassanelmi66@users.noreply.github.com>
2021-08-28 09:37:28 +0200

    Add files via upload

---
+++ langue.js
@@ -0,0 +1,340 @@
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#acc").text("Accueill");
+    });
+    
+    
+});
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#acc").text("Home");
+    });
+    
+    
+});
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#qui").text("Qui suis-je ?");
+    });
+    
+    
+});
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#qui").text("Who am I ?");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#edu").text("Education");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#comp").text("Competence");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#comp").text("My Skill");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#ser").text("Mes Services");
+    });
+    
+    
+});
+
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#ser").text("My Services");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#pro").text("Mes Projets");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#pro").text("My Projects");
+    });
+    
+    
+});
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#con").text("Me Contacter");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#con").text("Contact me");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#qui1").text("Qui suis-je ?");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#qui1").text("Who am I ?");
+    });
+    
+    
+});
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#mot").text("En quelque mot ");
+    });
+    
+    
+});
+
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#mot").text("In a few words ");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#ph1").text("Bonjour, je m’appelle Hassan Elmi, développeur Web junior et Designer  et je possède maintenant les compétences basiques permettant de créer des applications web fonctionnelles ainsi que des  site web ou des Application android. ");
+    
+     
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#ph1").text("Hello, my name is Hassan Elmi, Junior Web Developer and Designer and I now have the basic skills to create functional web applications as well as websites or android applications.");
+    
+     
+    });
+    
+    
+});
+
+
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#ph2").text(" Diplômé en Licence informatique a l'universite de Djibouti apres six semestres,cela m'a permis de travailler sur des projets web personnels et d'approfondir mes connaissances dans les langages Front-End et Back-End. Je suis actuellement à la recherche d'un emploi à plein temps en tant que développeur web.");
+     
+     
+    
+     
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#ph2").text(" Graduated in Computer Science at the University of Djibouti after six semesters, this allowed me to work on personal web projects and to deepen my knowledge in Front-End and Back-End languages. I am currently looking for a full time job as a web developer.");
+     
+     
+    
+     
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#log").text("Mes Logiciels ");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#log").text("My software ");
+    });
+    
+    
+});
+
+
+
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#sys").text("Mes Systeme d'exploitations :");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#sys").text("My Operating System:");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#comp1").text("Competence");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#comp1").text("Skill");
+    });
+    
+    
+});
+
+
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#ser1").text("Mes Services");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#ser1").text("My Service");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#pro1").text("Mes Projets");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#pro1").text("My  Project");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang1").click(function(){
+     $("#con1").text("Me Contacter");
+    });
+    
+    
+});
+
+
+$(function()
+{
+    $("#lang2").click(function(){
+     $("#con1").text("Conctat me");
+    });
+    
+    
+});
